d4a14f1885038e07aa446f5898127314935b829c
jsonlaiii <jasonlai661@gmail.com>
2025-11-18 15:23:01 -0700

    Added more data to the collection

--- data.js
+++ data.js
@@ -61,5 +61,121 @@ const labubuData = [
         series: "Time to Chill",
         image: "https://placeholder.com/labubu9",
         rarity: "Common"
+    },
+    // Have a Seat Series
+    {
+        id: 10,
+        name: "Labubu - Sitting",
+        series: "Have a Seat",
+        image: "https://placeholder.com/labubu10",
+        rarity: "Common"
+    },
+    {
+        id: 11,
+        name: "Labubu - Hehe",
+        series: "Have a Seat",
+        image: "https://placeholder.com/labubu11",
+        rarity: "Common"
+    },
+    {
+        id: 12,
+        name: "Labubu - Dada",
+        series: "Have a Seat",
+        image: "https://placeholder.com/labubu12",
+        rarity: "Common"
+    },
+    {
+        id: 13,
+        name: "Labubu - Ququ",
+        series: "Have a Seat",
+        image: "https://placeholder.com/labubu13",
+        rarity: "Common"
+    },
+    {
+        id: 14,
+        name: "Labubu - Zizi",
+        series: "Have a Seat",
+        image: "https://placeholder.com/labubu14",
+        rarity: "Common"
+    },
+    {
+        id: 15,
+        name: "Labubu - Baba",
+        series: "Have a Seat",
+        image: "https://placeholder.com/labubu15",
+        rarity: "Common"
+    },
+    {
+        id: 16,
+        name: "Labubu - DuoDuo",
+        series: "Have a Seat",
+        image: "https://placeholder.com/labubu16",
+        rarity: "Secret"
+    },
+    // Space Adventures
+    {
+        id: 17,
+        name: "Labubu Astronaut",
+        series: "Space Adventures",
+        image: "https://placeholder.com/labubu17",
+        rarity: "Common"
+    },
+    {
+        id: 18,
+        name: "Labubu Alien",
+        series: "Space Adventures",
+        image: "https://placeholder.com/labubu18",
+        rarity: "Common"
+    },
+    {
+        id: 19,
+        name: "Labubu Commander",
+        series: "Space Adventures",
+        image: "https://placeholder.com/labubu19",
+        rarity: "Secret"
+    },
+    // Camping
+    {
+        id: 20,
+        name: "Labubu Hiker",
+        series: "The Monsters Camping",
+        image: "https://placeholder.com/labubu20",
+        rarity: "Common"
+    },
+    {
+        id: 21,
+        name: "Labubu Kayak",
+        series: "The Monsters Camping",
+        image: "https://placeholder.com/labubu21",
+        rarity: "Common"
+    },
+    {
+        id: 22,
+        name: "Labubu Bonfire",
+        series: "The Monsters Camping",
+        image: "https://placeholder.com/labubu22",
+        rarity: "Secret"
+    },
+    // Art Series
+    {
+        id: 23,
+        name: "Labubu Van Gogh",
+        series: "The Monsters Art",
+        image: "https://placeholder.com/labubu23",
+        rarity: "Common"
+    },
+    {
+        id: 24,
+        name: "Labubu Da Vinci",
+        series: "The Monsters Art",
+        image: "https://placeholder.com/labubu24",
+        rarity: "Common"
+    },
+    {
+        id: 25,
+        name: "Labubu The Scream",
+        series: "The Monsters Art",
+        image: "https://placeholder.com/labubu25",
+        rarity: "Secret"
     }
 ];
